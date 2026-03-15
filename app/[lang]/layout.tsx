8c73747f0a94a072cd871e99abaf722470309735
Ace <PC@HONGCF>
2026-03-15 21:46:32 +0800

    t

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import { LanguageProvider } from '@/contexts/LanguageContext'
+import type { Language } from '@/contexts/LanguageContext'
 
 export default async function LangLayout({
   children,
@@ -12,7 +13,7 @@ export default async function LangLayout({
   const { lang } = await params
   
   return (
-    <LanguageProvider lang={lang}>
+    <LanguageProvider lang={lang as Language}>
       <Navbar />
       {children}
       <Footer />
